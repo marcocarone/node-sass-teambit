8dea417c8bf6f4535ac9477febd7878bbc76dffb
Marco <marco.carone@libero.it>
2020-02-18 20:02:44 +0100

    aggiunto js

---
+++ src/js/script.js
@@ -0,0 +1,36 @@
+var $ = require("jquery");
+
+$(window).scroll(function() {
+  var scroll = $(window).scrollTop();
+
+
+  if (scroll >= 550) {
+
+    $(".top-header").addClass("fix");
+    $(".top-header").addClass("fade-in-top");
+    $(".top-header__wrapper").css("border-bottom", "none");
+    $(".btn__menu").addClass("btn__scroll");
+    $(".btn__menu").css("color", "rgb(255, 255, 255)");
+  } else {
+    $(".top-header").removeClass("fix");
+    $(".top-header").removeClass("fade-in-top");
+    $(".top-header__wrapper").css("border-bottom", "solid 1px rgb(144, 144, 144)");
+    $(".btn__menu").removeClass("btn__scroll");
+    $(".btn__menu").css("color", "rgb(48, 72, 143)");
+    $(".btn__menu:hover").css("color", "rgb(255, 255, 255)");
+  }
+});
+
+$(document).ready( function() {
+
+    $(".parent__drop-down").hover(
+      function() {
+        $(".drop-down").addClass("active")
+      });
+
+    $(".parent__drop-down").mouseleave(
+      function() {
+        $(".drop-down").removeClass("active")
+      })
+  }
+)
